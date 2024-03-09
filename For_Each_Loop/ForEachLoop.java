d39d1b58f8af64de334bb3ac7ca1c70b24c61e13
Md. Rakibul Asib Redoy <126803555+Asib177@users.noreply.github.com>
2024-03-09 23:53:21 +0600

    Add files via upload

---
+++ For_Each_Loop/ForEachLoop.java
@@ -0,0 +1,7 @@
+public class ForEachLoop {
+    public static void main(String[] args) {
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        for(String i : cars)
+        System.out.println(i);
+    }
+}
