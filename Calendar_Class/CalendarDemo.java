6016b0092719f3b2e62a7d8b92b223f494115300
Md. Rakibul Asib Redoy <126803555+Asib177@users.noreply.github.com>
2024-03-12 05:38:24 +0600

    Add files via upload

---
+++ Calendar_Class/CalendarDemo.java
@@ -0,0 +1,20 @@
+package Calendar_Class;
+
+import java.util.Calendar;
+
+public class CalendarDemo {
+    public static void main(String[] args) {
+        Calendar date = Calendar.getInstance();
+        // System.out.println(date.toString());
+        // System.out.println(date.getInstance());
+        // System.out.println(date.getTime());
+        // System.out.println(date.getTimeZone());
+        // System.out.println(date.after(date));
+        // System.out.println(date.get(0));
+        // System.out.println(date.equals(date));
+        // date.set(Calendar.YEAR,2001 );
+        // System.out.println(date.getTime());
+        // date.set(2023, 2, 7, 8, 19, 21);
+        System.out.println(date.getTime());
+    }
+}
