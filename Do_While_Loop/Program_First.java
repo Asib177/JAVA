9d64c3fb1ac5e606fbd4f17d595785eaf241e105
Md. Rakibul Asib Redoy <126803555+Asib177@users.noreply.github.com>
2024-03-11 22:43:09 +0600

    Add files via upload

---
+++ Do_While_Loop/Program_First.java
@@ -0,0 +1,9 @@
+public class Program_First {
+    public static void main(String[] args) {
+        int i=0;
+        do {
+            System.out.println(i);
+            i++;
+        } while(i<5);
+    }
+}
