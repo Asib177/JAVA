bb72e692571dbf01b4ac0916baf71c3e36a5edce
Md. Rakibul Asib Redoy <126803555+Asib177@users.noreply.github.com>
2024-03-14 04:55:41 +0600

    Add files via upload

---
+++ New folder/projct3.java
@@ -0,0 +1,14 @@
+class project3{
+
+    String projectname;
+    int teamMember[];
+    int nTeam =0;
+    boolean rS = false;
+
+    void Member(){
+
+    }
+    public static void main(String[] args) {
+    
+    }
+}
